2675755cce6f2ed6967e2ce05bc3371a3e7b1398
ezequiel <quielneres@gmail.com>
2020-05-06 21:53:35 -0300

    boletos resposne

--- src/pages/enterValue/response/index.js
+++ src/pages/enterValue/response/index.js
@@ -1,61 +1,84 @@
-import React, {useEffect, useState} from 'react';
-import {View, Image, StyleSheet, Alert, TouchableOpacity} from "react-native";
-import {Container, Content, Right, Body, Footer, ActionSheet, Button} from "native-base";
-import {Card, Header, Badge, Text} from 'react-native-elements'
-import colors from "../../../components/styles/colors";
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import MaskService from "react-native-masked-text/lib/mask-service";
+import React, {Component, useState} from 'react'
+import {View, Text, StyleSheet, Share} from 'react-native'
+import {ActionSheetCustom as ActionSheet} from 'react-native-custom-actionsheet';
+import Clipboard from "@react-native-community/clipboard";
+import {Button} from "react-native-elements";
+import {Content} from "native-base";
 
 
-const Response = ({navigation}) => {
-    var BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
-    var DESTRUCTIVE_INDEX = 3;
-    var CANCEL_INDEX = 4;
-
-    const [data_trasaction] = useState(navigation.getParam('data'));
-    const [data_response] = useState(navigation.getParam('data_response'));
-    const [option_btn, setOptionBtn] = useState({
-        clicked: null
-    });
+const CANCEL_INDEX = 0;
+const DESTRUCTIVE_INDEX = 5;
+const options = [
+    'Cancelar',
+    'Copiar digitos',
+    'Compartilhar link de pagamento'
+];
 
 
+class CustomExample extends Component {
+    state = {
+        selected: 1,
+    };
+
+    showActionSheet = () => this.actionSheet.show();
+
+    getActionSheetRef = ref => (this.actionSheet = ref);
+
+    handlePress = index => {
+
+        console.log(index);
+        console.log( Clipboard.getString());
+
+        { index === 1 ? Clipboard.getString() : null}
+
+        {
+            index === 2 ?
+                Share.share(
+                    {
+                        title: "a title",
+                        // message: "some message",
+                        // or
+                        url: 'https://reactnativeexample.com/tag/share/'
+                    },
+                    (options)
+                ) : null
+        }
+
+        this.setState({selected: index})
+    };
 
 
-    return (
-        <Container>
-            <Header
-                containerStyle={{backgroundColor: colors.primary}}
-                leftComponent={<Icon name={'chevron-left'} color={'#fff'} size={25} onPress={() => navigation.goBack(null)}/>}
-                centerComponent={{ text: data_trasaction.title, style: {fontSize: 16, color: '#fff', fontWeight: 'bold' }}}
-            />
-            <View style={{padding: 20, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>{data_response.barCode}</Text>
+    render() {
+
+        const {selected} = this.state;
+        const selectedText = options[selected].component || options[selected];
+
+        return (
+            <View>
+                <Text style={{marginBottom: 20}}>
+                    I like {selectedText}
+                </Text>
+
+
+                <View style={{alignItems: 'center'}}>
+                    <Button
+                        buttonStyle={{width: 300, justifyContent: 'center', backgroundColor: '#4CB1F7', marginTop: 20}}
+                        onPress={this.showActionSheet}
+                        title="Opçoes do boleto"
+                    />
+                </View>
+                <ActionSheet
+                    ref={this.getActionSheetRef}
+                    // title={title}
+                    // message="custom message custom message custom message custom message custom message custom message "
+                    options={options}
+                    cancelButtonIndex={CANCEL_INDEX}
+                    destructiveButtonIndex={DESTRUCTIVE_INDEX}
+                    onPress={this.handlePress}
+                />
             </View>
-            <View style={{padding: 20, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Valor: {MaskService.toMask('money', data_trasaction.value)}</Text>
-                <Text>Vanecimento: {data_response.expire}</Text>
-            </View>
-            <View style={{padding: 20, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Uma copia desse boleto foi enviado par o email quielneres@gmail.com</Text>
-                <Button
-                    onPress={() =>
-                        ActionSheet.show(
-                            {
-                                options: BUTTONS,
-                                cancelButtonIndex: CANCEL_INDEX,
-                                destructiveButtonIndex: DESTRUCTIVE_INDEX,
-                                title: "Testing ActionSheet"
-                            },
-                            buttonIndex => {
-                                setOptionBtn({ clicked: BUTTONS[buttonIndex] });
-                            }
-                        )}
-                >
-                    <Text>Opçoes do boleto</Text>
-                </Button>
-            </View>
-        </Container>
-    );
-};
+        )
+    }
+}
 
-export default Response;
+export default CustomExample;
